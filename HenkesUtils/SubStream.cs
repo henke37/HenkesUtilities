4df39a2b2c62c8e1fca7d07279ca03ea51719b93
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2018-09-15 21:39:13 +0200

    Consistent indentation

--- HenkesUtils/SubStream.cs
+++ HenkesUtils/SubStream.cs
@@ -6,86 +6,86 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HenkesUtils {
-    public class SubStream : Stream {
-        private Stream stream;
+	public class SubStream : Stream {
+		private Stream stream;
 
-        public long offset;
-        public long limit;
+		public long offset;
+		public long limit;
 
-        private long currentPosition;
+		private long currentPosition;
 
-        public SubStream(SubStream baseSub, long offset, long limit = long.MaxValue) {
-            if(offset < 0) throw new ArgumentOutOfRangeException("The offset can't be negative!", nameof(offset));
-            this.offset = offset;
-            if(limit < 0) throw new ArgumentOutOfRangeException("The limit can't be negative!", nameof(limit));
-            this.limit = limit;
+		public SubStream(SubStream baseSub, long offset, long limit = long.MaxValue) {
+			if(offset < 0) throw new ArgumentOutOfRangeException("The offset can't be negative!", nameof(offset));
+			this.offset = offset;
+			if(limit < 0) throw new ArgumentOutOfRangeException("The limit can't be negative!", nameof(limit));
+			this.limit = limit;
 
-            FromBaseSubStream(baseSub, offset, limit);
-        }
+			FromBaseSubStream(baseSub, offset, limit);
+		}
 
-        private void FromBaseSubStream(SubStream baseSub, long offset, long limit) {
-            this.stream = baseSub.stream;
-            this.offset = baseSub.offset + offset;
-            this.limit = (limit < baseSub.limit ? limit : baseSub.limit);
-        }
+		private void FromBaseSubStream(SubStream baseSub, long offset, long limit) {
+			this.stream = baseSub.stream;
+			this.offset = baseSub.offset + offset;
+			this.limit = (limit < baseSub.limit ? limit : baseSub.limit);
+		}
 
-        public SubStream(Stream stream, long offset, long limit = long.MaxValue) {
-            if(offset < 0) throw new ArgumentOutOfRangeException(nameof(offset), "The offset can't be negative!");
-            this.offset = offset;
-            if(limit < 0) throw new ArgumentOutOfRangeException(nameof(limit), "The limit can't be negative!");
-            this.limit = limit;
+		public SubStream(Stream stream, long offset, long limit = long.MaxValue) {
+			if(offset < 0) throw new ArgumentOutOfRangeException(nameof(offset), "The offset can't be negative!");
+			this.offset = offset;
+			if(limit < 0) throw new ArgumentOutOfRangeException(nameof(limit), "The limit can't be negative!");
+			this.limit = limit;
 
-            {
-                var baseSub = stream as SubStream;
-                if(baseSub != null) {
-                    FromBaseSubStream(baseSub, offset, limit);
-                    return;
-                }
-            }
-            this.stream = stream ?? throw new ArgumentNullException(nameof(stream));
-            if(!stream.CanSeek) throw new ArgumentException("The stream must be seekable!", nameof(stream));
+			{
+				var baseSub = stream as SubStream;
+				if(baseSub != null) {
+					FromBaseSubStream(baseSub, offset, limit);
+					return;
+				}
+			}
+			this.stream = stream ?? throw new ArgumentNullException(nameof(stream));
+			if(!stream.CanSeek) throw new ArgumentException("The stream must be seekable!", nameof(stream));
 
 
-        }
+		}
 
-		public SubStream(BinaryReader r, long limit=long.MaxValue) : this(r.BaseStream, r.BaseStream.Position, limit) {
+		public SubStream(BinaryReader r, long limit = long.MaxValue) : this(r.BaseStream, r.BaseStream.Position, limit) {
 		}
 		public SubStream(BinaryWriter w, long limit = long.MaxValue) : this(w.BaseStream, w.BaseStream.Position, limit) {
 		}
 
 		public override bool CanRead => stream.CanRead;
-        public override bool CanSeek => true;
-        public override bool CanWrite => stream.CanWrite;
-        public override bool CanTimeout => stream.CanTimeout;
+		public override bool CanSeek => true;
+		public override bool CanWrite => stream.CanWrite;
+		public override bool CanTimeout => stream.CanTimeout;
 
-        public override long Length {
-            get {
-                var uncappedLen = stream.Length - offset;
-                if(uncappedLen > limit) return limit;
-                return uncappedLen;
-            }
-        }
+		public override long Length {
+			get {
+				var uncappedLen = stream.Length - offset;
+				if(uncappedLen > limit) return limit;
+				return uncappedLen;
+			}
+		}
 
-        public override long Position { set => currentPosition = value; get => currentPosition; }
+		public override long Position { set => currentPosition = value; get => currentPosition; }
 
-        public override void Flush() {
-            stream.Flush();
-        }
+		public override void Flush() {
+			stream.Flush();
+		}
 
-        public override int Read(byte[] buffer, int bufferWriteOffset, int count) {
-            if(buffer == null) throw new ArgumentNullException(nameof(buffer));
-            if(bufferWriteOffset < 0) throw new ArgumentOutOfRangeException(nameof(bufferWriteOffset));
-            if(count < 0) throw new ArgumentOutOfRangeException(nameof(count));
-            if(bufferWriteOffset + count > buffer.Length) throw new ArgumentException("Invalid range in buffer specified");
+		public override int Read(byte[] buffer, int bufferWriteOffset, int count) {
+			if(buffer == null) throw new ArgumentNullException(nameof(buffer));
+			if(bufferWriteOffset < 0) throw new ArgumentOutOfRangeException(nameof(bufferWriteOffset));
+			if(count < 0) throw new ArgumentOutOfRangeException(nameof(count));
+			if(bufferWriteOffset + count > buffer.Length) throw new ArgumentException("Invalid range in buffer specified");
 
-            stream.Seek(currentPosition + offset, SeekOrigin.Begin);
-            int bytesToRead = count;
-            if(bytesToRead > Length) bytesToRead = (int)Length;
-            int bytesRead = stream.Read(buffer, bufferWriteOffset, bytesToRead);
-            currentPosition += bytesRead;
+			stream.Seek(currentPosition + offset, SeekOrigin.Begin);
+			int bytesToRead = count;
+			if(bytesToRead > Length) bytesToRead = (int)Length;
+			int bytesRead = stream.Read(buffer, bufferWriteOffset, bytesToRead);
+			currentPosition += bytesRead;
 
-            return bytesRead;
-        }
+			return bytesRead;
+		}
 
 		public override void Write(byte[] buffer, int bufferReadOffset, int count) {
 			if(buffer == null) throw new ArgumentNullException(nameof(buffer));
@@ -103,23 +103,23 @@ namespace HenkesUtils {
 		}
 
 		public override long Seek(long offset, SeekOrigin origin) {
-            switch(origin) {
-                case SeekOrigin.Begin:
-                    return currentPosition = offset;
-                case SeekOrigin.Current:
-                    return currentPosition += offset;
-                case SeekOrigin.End:
-                    return currentPosition = Length + offset;
-                default:
-                    throw new ArgumentException();
-            }
-        }
+			switch(origin) {
+				case SeekOrigin.Begin:
+					return currentPosition = offset;
+				case SeekOrigin.Current:
+					return currentPosition += offset;
+				case SeekOrigin.End:
+					return currentPosition = Length + offset;
+				default:
+					throw new ArgumentException();
+			}
+		}
 
-        public override void SetLength(long value) {
-            throw new NotSupportedException();
-        }
+		public override void SetLength(long value) {
+			throw new NotSupportedException();
+		}
 
-        public override int ReadTimeout { get => stream.ReadTimeout; set => stream.ReadTimeout = value; }
-        public override int WriteTimeout { get => stream.WriteTimeout; set => stream.WriteTimeout = value; }
-    }
+		public override int ReadTimeout { get => stream.ReadTimeout; set => stream.ReadTimeout = value; }
+		public override int WriteTimeout { get => stream.WriteTimeout; set => stream.WriteTimeout = value; }
+	}
 }
