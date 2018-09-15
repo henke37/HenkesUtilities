dfd5380d1c01de4c8a8f2b7313241824ff67adde
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2018-09-15 21:04:56 +0200

    Proper Assembly info

--- HenkesUtils/Properties/AssemblyInfo.cs
+++ HenkesUtils/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HenkesUtils")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Misc utility library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Henke37")]
 [assembly: AssemblyProduct("HenkesUtils")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Henke37 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
