41f2f32592eb6684eca1f583c6e96a80ccc9b7c9
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-07-09 01:30:02 +0200

    Validate that the retrived path exists

--- AppInstallDirTest/Program.cs
+++ AppInstallDirTest/Program.cs
@@ -1,12 +1,17 @@
 ﻿using Henke37.Valve.Steam;
 using System;
+using System.IO;
 
 namespace AppInstallDirTest {
 	class Program {
-		static void Main(string[] args) {
+		static int Main(string[] args) {
 			int appId = int.Parse(args[0]);
 			string path=SteamHelper.GetInstallPathForApp(appId);
+			if(!Directory.Exists(path)) {
+				return 1;
+			}
 			Console.WriteLine(path);
+			return 0;
 		}
 	}
 }
