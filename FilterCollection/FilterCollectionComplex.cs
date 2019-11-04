087f7ec3d3abb508ae72ff25d0cfda224f89837f
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-11-04 21:01:22 +0100

    Filter collection to take care of filtering and notifying on changes to the generated collection.

---
+++ FilterCollection/FilterCollectionComplex.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Henke37.Collections.Filtered {
+	public class FilterCollection<TItem, TArg> : INotifyCollectionChanged, IReadOnlyCollection<TItem> {
+		private IReadOnlyCollection<TItem> realCollection;
+
+		public event NotifyCollectionChangedEventHandler CollectionChanged;
+		private Func<TItem, TArg, bool> filterFunc;
+		private TArg filterArg;
+
+		public FilterCollection(IReadOnlyCollection<TItem> realCollection, Func<TItem, TArg, bool> filterFunc) {
+			this.realCollection = realCollection ?? throw new ArgumentNullException(nameof(realCollection));
+			this.filterFunc = filterFunc ?? throw new ArgumentNullException(nameof(filterFunc));
+		}
+
+		public FilterCollection(IReadOnlyCollection<TItem> realCollection, Func<TItem, TArg, bool> filterFunc, TArg filterArg) {
+			this.realCollection = realCollection ?? throw new ArgumentNullException(nameof(realCollection));
+			this.filterFunc = filterFunc ?? throw new ArgumentNullException(nameof(filterFunc));
+			this.filterArg = filterArg;
+		}
+
+		public Func<TItem, TArg, bool> FilterFunc {
+			get => filterFunc;
+			set {
+				if(value == filterFunc) return;
+				filterFunc = value;
+				ListChanged();
+			}
+		}
+
+		public TArg FilterArg {
+			get => filterArg;
+			set {
+				filterArg = value;
+				ListChanged();
+			}
+		}
+
+		public IReadOnlyCollection<TItem> RealCollection {
+			get => realCollection;
+			set {
+				realCollection = value;
+				ListChanged();
+			}
+		}
+
+		private void ListChanged() {
+			CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+		}
+
+		public int Count => realCollection.Count(I => filterFunc(I, filterArg));
+
+		public IEnumerator<TItem> GetEnumerator() {
+			foreach(var item in realCollection) {
+				if(!filterFunc(item, FilterArg)) continue;
+				yield return item;
+			}
+			yield break;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+	}
+}
---
+++ FilterCollection/FilterCollectionSimple.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Henke37.Collections.Filtered {
+	public class FilterCollection<TItem> : INotifyCollectionChanged, IReadOnlyCollection<TItem> {
+		private IReadOnlyCollection<TItem> realCollection;
+
+		public event NotifyCollectionChangedEventHandler CollectionChanged;
+		private Func<TItem, bool> filterFunc;
+
+		public FilterCollection(IReadOnlyCollection<TItem> realCollection, Func<TItem, bool> filterFunc) {
+			this.realCollection = realCollection ?? throw new ArgumentNullException(nameof(realCollection));
+			this.filterFunc = filterFunc ?? throw new ArgumentNullException(nameof(filterFunc));
+		}
+
+		public Func<TItem, bool> FilterFunc {
+			get => filterFunc;
+			set {
+				if(value == filterFunc) return;
+				filterFunc = value;
+				ListChanged();
+			}
+		}
+
+		public IReadOnlyCollection<TItem> RealCollection {
+			get => realCollection;
+			set {
+				realCollection = value;
+				ListChanged();
+			}
+		}
+
+		private void ListChanged() {
+			CollectionChanged?.Invoke(this,new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+		}
+
+		public int Count => realCollection.Count(filterFunc);
+
+		public IEnumerator<TItem> GetEnumerator() {
+			foreach(var item in realCollection) {
+				if(!filterFunc(item)) continue;
+				yield return item;
+			}
+			yield break;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+	}
+}
